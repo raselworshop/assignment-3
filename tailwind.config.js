ce8f2d30c3d79bf664ae601f04059ddb00269445
raselworshop <raselworshop@gmail.com>
2024-08-21 14:24:25 -0400

    added the banner section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
       colors: {
         clifford: '#da373d',
         'btn-my': '#ABEF5F',
+        'btn-my-bg': 'rgba(171, 239, 95, 0.5)',
+        'title-my': 'rgba(33, 33, 33, 1)',
       }
     },
   },
