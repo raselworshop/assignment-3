8eaa935078977115ae2feed07d6a77cd7a58b6a7
raselworshop <raselworshop@gmail.com>
2024-08-21 13:12:32 -0400

    added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {},
+    fontFamily: {
+      'manrope': ['"Manrope"', '"sans-serif"']
+    },
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        'btn-my': '#ABEF5F',
+      }
+    },
   },
   plugins: [require('daisyui')],
 }
